d74898454db16fcc637b3003a449021740a16860
Toni Stoyanov <toni.n.stoyanov@icloud.com>
2025-02-04 21:06:01 +0200

    Initial Commit

---
+++ SwiftAdvanced/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftAdvanced
+//
+//  Created by Toni Stoyanov on 4.02.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftAdvanced/SwiftAdvancedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAdvancedApp.swift
+//  SwiftAdvanced
+//
+//  Created by Toni Stoyanov on 4.02.25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAdvancedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
