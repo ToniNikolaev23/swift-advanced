15d60ab929e7d6599c419451d8f6da37b7df8abd
Toni Stoyanov <toni.n.stoyanov@icloud.com>
2025-02-07 23:03:19 +0200

    Add custom shapes

---
+++ SwiftAdvanced/CustomShapesBootcamp.swift
@@ -0,0 +1,70 @@
+//
+//  CustomShapesBootcamp.swift
+//  SwiftAdvanced
+//
+//  Created by Toni Stoyanov on 7.02.25.
+//
+
+import SwiftUI
+
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        }
+    }
+}
+
+struct Diamond: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            let horizontalOffset: CGFloat = rect.width * 0.2
+            path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX - horizontalOffset, y: rect.midY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX + horizontalOffset, y: rect.midY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        }
+    }
+}
+
+struct Trapezoid: Shape {
+    func path(in rect: CGRect) -> Path {
+        Path { path in
+            let horizontalOffset: CGFloat = rect.width * 0.2
+
+            path.move(to: CGPoint(x: rect.minX + horizontalOffset, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX - horizontalOffset, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX + horizontalOffset, y: rect.minY))
+        }
+    }
+}
+
+struct CustomShapesBootcamp: View {
+    var body: some View {
+        ZStack {
+//            Image("pic1")
+//                .resizable()
+//                .scaledToFill()
+//            Triangle()
+//            Diamond()
+            Trapezoid()
+//                .trim(from: 0, to: 0.5)
+//                .fill(LinearGradient(colors: [Color.red, Color.blue], startPoint: .leading, endPoint: .trailing))
+                .frame(width: 300, height: 300)
+//                .clipShape(
+//                    Triangle()
+//                        .rotation(Angle(degrees: 180))
+//                )
+        }
+    }
+}
+
+#Preview {
+    CustomShapesBootcamp()
+}
